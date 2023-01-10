e573603962c0e87a0bf9c0e7ee1f1413a3d37821
Linzy <y.liashchuk@dev-branch.com>
2023-01-10 16:03:52 +0200

    #5834 Added custom module for displaying image right or left inside the block and added styles for it.

---
+++ web/modules/custom/swap_blocks/src/Plugin/paragraphs/Behavior/ImageAndTextBehavior.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\swap_blocks\Plugin\paragraphs\Behavior;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\Entity\ParagraphsType;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\paragraphs\ParagraphsBehaviorBase;
+
+/**
+ * @ParagraphsBehavior(
+ *   id = "dlog_paragraphs_image_and_text",
+ *   label = @Translation("Image and text settings"),
+ *   description= @Translation("Settings for image and text paragraph type."),
+ *   weight = 0,
+ * )
+ */
+class ImageAndTextBehavior extends ParagraphsBehaviorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function isApplicable(ParagraphsType $paragraphs_type) {
+    return $paragraphs_type->id() == 'image_and_text';
+  }
+
+  /**
+   * Extends the paragraph render array with behavior.
+   */
+  public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode) {
+    $image_position = $paragraph->getBehaviorSetting($this->getPluginId(), 'image_position', 'left');
+    $build['#attributes']['class'][] = Html::getClass('paragraph-' . 'image-position-' . $image_position);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state) {
+    $form['image_position'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Image position'),
+      '#options' => [
+        'left' => $this->t('Left'),
+        'right' => $this->t('Right'),
+      ],
+      '#default_value' => $paragraph->getBehaviorSetting($this->getPluginId(), 'image_position', 'left'),
+    ];
+
+    return $form;
+  }
+
+}
