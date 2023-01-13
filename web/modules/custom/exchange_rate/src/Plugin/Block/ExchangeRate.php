547e67d7b9b0020111a658da970b683e14caa29c
Linzy <y.liashchuk@dev-branch.com>
2023-01-13 17:28:28 +0200

    #5841 Used dependence objection in exchange rate block

--- web/modules/custom/exchange_rate/src/Plugin/Block/ExchangeRate.php
+++ web/modules/custom/exchange_rate/src/Plugin/Block/ExchangeRate.php
@@ -3,9 +3,12 @@
 namespace Drupal\exchange_rate\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\exchange_rate\BlockServices;
 
 /**
- * Provides a 'Hello' Block.
+ * Provides a 'Exchange rate' Block.
  *
  * @Block(
  *   id = "exchange_rate",
@@ -13,22 +16,45 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Custom"),
  * )
  */
-class ExchangeRate extends BlockBase {
+class ExchangeRate extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\exchange_rate\BlockServices
+   */
+  protected $service;
+
+  /**
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   * @param  \Drupal\exchange_rate\BlockServices $service
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, BlockServices $service) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->service = $service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('exchange_rate.api_connector')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $url = \Drupal::config('exchange_rate.admin_settings')->get('url');
-    $client = \Drupal::httpClient();
-    $request = $client->get($url);
-    $response = $request->getBody();
-
-    $decoded_json = json_decode($response->getContents(), TRUE);
+    $list = $this->service->getUrl();
 
     return [
       '#theme' => 'block--exchange-rate',
-      '#data' => $decoded_json,
+      '#data' => $list,
     ];
   }
 
